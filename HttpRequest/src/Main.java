20da8f766e0ede3c38e1c29a0fba04c9667a5747
Yahel Nachum <ynachum@wpi.edu>
2016-09-18 15:33:59 -0400

    Got post in java working for Watson

--- HttpRequest/src/Main.java
+++ HttpRequest/src/Main.java
@@ -1,56 +1,113 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-	
-		URL url = new URL("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify");
-		URLConnection con = url.openConnection();
-		HttpURLConnection http = (HttpURLConnection)con;
-		http.setRequestMethod("POST"); // PUT is another valid option
-		http.setDoOutput(true);
+	public static void main(String[] args) throws Exception {
+		Main.sendPostWatson();
 		
-		Map<String,String> arguments = new HashMap<>();
-		arguments.put("api_key", "68afcccf311899e6f6cc6064de624901456c180a");
-		StringJoiner sj = new StringJoiner("&");
-		for(Map.Entry<String,String> entry : arguments.entrySet()){
-		    sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "=" 
-		         + URLEncoder.encode(entry.getValue(), "UTF-8"));
-		    System.out.println(URLEncoder.encode(entry.getKey(), "UTF-8") + "=" 
-		         + URLEncoder.encode(entry.getValue(), "UTF-8"));
-		}
-		System.out.println(sj.toString());
-		byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
-		int length = out.length;
-		
-		http.setFixedLengthStreamingMode(length);
-		http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-		http.connect();
-		try(OutputStream os = http.getOutputStream()) {
-		    os.write(out);
-		}
-
-		InputStream is = http.getInputStream();
-		
-		System.out.println(is.available());
-		byte[] bytes = new byte[is.available()];
-		
-		is.read(bytes);
-
-		StringBuilder sb = new StringBuilder();
-		for(byte b: bytes){
-			sb.append((char) b);
-		}
-		System.out.println(sb.toString());
 	}
+
+	// HTTP GET request
+	private static void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+/*		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);*/
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	private static void sendPostWatson() throws Exception {
+
+		String url = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=68afcccf311899e6f6cc6064de624901456c180a&version=2016-05-20";
+		String apiString = "api_key";
+		String apiToken = "68afcccf311899e6f6cc6064de624901456c180a";
+		String versionString = "version";
+		String versionToken = "2016-05-20";
+		
+		String combineQuery = "&";
+		String combineToken = "=";
+		
+		String imagesString = "images_file";
+		String imagesToken = "C:/Users/yahel/Pictures/ManyCam/My Snapshot1.jpg";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+/*		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");*/
+		//con.setRequestProperty(imagesString, imagesToken);
+		
+		//String urlParameters = "form-data: name=\""+imagesString+"\"; file-name=\""+imagesToken+"\"\n\r";//imagesString+combineToken+imagesToken;//apiString+combineToken+apiToken;//+combineQuery+versionString+combineToken+versionToken+combineQuery+imagesString+combineToken+imagesToken;
+		
+		File image = new File(imagesToken);
+		System.out.println("file exists? "+image.exists());
+		
+		FileInputStream iStream = new FileInputStream(image);
+		byte[] bArray = new byte[(int) image.length()];
+		iStream.read(bArray);
+		iStream.close();
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		//wr.writeBytes(urlParameters);
+		wr.write(bArray);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		//System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
 }
