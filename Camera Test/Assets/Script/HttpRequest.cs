d694572a777bbc1114d45799f2b72e0c0774b408
Yahel Nachum <yahelnachum@yahoo.com>
2016-09-19 08:37:29 -0400

    Got Post working in Unity

--- Camera Test/Assets/Script/HttpRequest.cs
+++ Camera Test/Assets/Script/HttpRequest.cs
@@ -10,9 +10,24 @@ public class HttpRequest : MonoBehaviour {
         //WWW results = GET("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=68afcccf311899e6f6cc6064de624901456c180a&url=https://github.com/watson-developer-cloud/doc-tutorial-downloads/raw/master/visual-recognition/fruitbowl.jpg&version=2016-05-19");
         //Debug.Log("Results: " + results.text);
 
-        byte[] bytes = System.IO.File.ReadAllBytes(Application.dataPath + "/unityWebcam.jpg");
+        /*byte[] bytes = System.IO.File.ReadAllBytes(Application.dataPath + "/unityWebcam.jpg");
         WWW results = POST("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=68afcccf311899e6f6cc6064de624901456c180a&version=2016-05-20", "images_file", "@" + Application.dataPath + "/unityWebcam.jpg", "image/jpg", bytes);
+        Debug.Log("Results: " + results.text);*/
+
+        WWW results = PostWatson(Application.dataPath + "/unityWebcam.jpg");
         Debug.Log("Results: " + results.text);
+    }
+
+    WWW PostWatson(string pathToImage)
+    {
+        WWWForm form = new WWWForm();
+        //form.AddBinaryData("images_file", System.IO.File.ReadAllBytes(Application.dataPath + "/unityWebcam.jpg"));
+        //form.AddBinaryData("images_file", System.IO.File.ReadAllBytes(Application.dataPath + "/unityWebcam.jpg"), Application.dataPath + "/unityWebcam.jpg");
+        //form.AddBinaryData("images_file", System.IO.File.ReadAllBytes(Application.dataPath + "/unityWebcam.jpg"), Application.dataPath + "/unityWebcam.jpg", "multipart/form-data");
+        WWW www = new WWW("https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=68afcccf311899e6f6cc6064de624901456c180a&version=2016-05-20", System.IO.File.ReadAllBytes(pathToImage));// form);
+        StartCoroutine(WaitForRequest(www));
+
+        return www;
     }
 
     // Update is called once per frame
@@ -23,15 +38,6 @@ public class HttpRequest : MonoBehaviour {
     public WWW GET(string url)
     {
         WWW www = new WWW(url);
-        StartCoroutine(WaitForRequest(www));
-        return www;
-    }
-
-    public WWW POST(string url, string key, string value, string type, byte[] bytes)
-    {
-        WWWForm form = new WWWForm();
-        form.AddBinaryData(key, bytes, value, type);
-        WWW www = new WWW(url, form);
         StartCoroutine(WaitForRequest(www));
         return www;
     }
--- Camera Test/Assets/Script/VisualRecognitionService.cs
+++ Camera Test/Assets/Script/VisualRecognitionService.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class VisualRecognitionService : MonoBehaviour {
@@ -30,3 +30,4 @@ public class VisualRecognitionService : MonoBehaviour {
 	
 	}
 }
+*/
